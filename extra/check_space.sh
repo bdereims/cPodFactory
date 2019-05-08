58d567896fa2593cf37a6bbfaa43363397ae5516
Brice Dereims <bdereims@vmware.com>
2019-05-08 07:34:05 +0000

    mod check_space

--- extra/check_space.sh
+++ extra/check_space.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-exit 0
+
 . ./govc_env
 . ./env
 
 CLUSTER=$( echo $CLUSTER | tr '[:lower:]' '[:upper:]' )
 
-DELLVSAN=$( govc datastore.info ${CLUSTER}-VSAN | grep Free | sed -e "s/^.*://" -e "s/GB//" -e "s/ //g" )
-DELLVSAN=$( echo "${DELLVSAN}/1" | bc )
-DELLVSAN=$( expr ${DELLVSAN} )
+VSAN=$( govc datastore.info ${CLUSTER}-VSAN | grep Free | sed -e "s/^.*://" -e "s/GB//" -e "s/ //g" )
+VSAN=$( echo "${DELLVSAN}/1" | bc )
+VSAN=$( expr ${DELLVSAN} )
 
-MEM=$( govc metric.sample "host/INTEL Cluster" mem.usage.average | sed -e "s/,.*$//" | cut -f10 -d" " | cut -f1 -d"." )
-#govc metric.sample "host/DELL Cluster" mem.usage.average | sed "s/^.*,//" | cut -f1 -d" " | cut -f1 -d"." )
+MEM=$( govc metric.sample "host/${CLUSTER} Cluster" mem.usage.average | sed -e "s/,.*$//" | cut -f10 -d" " | cut -f1 -d"." )
 MEM=$( expr ${MEM} )
 
-if [ 10000 -gt ${DELLVSAN} ] || [ 80 -lt ${MEM} ]
+if [ 10000 -gt ${VSAN} ] || [ 80 -lt ${MEM} ]
 then
 	echo "No more space!"
 	exit 1
