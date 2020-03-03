7f2cb1c551a9fe022d9f025377b4564442043aa0
Brice Dereims <bdereims@vmware.com>
2020-03-03 17:00:55 +0000

    new name generator

--- install/vlan.sh
+++ install/vlan.sh
@@ -3,6 +3,8 @@
 
 INTERFACE=eth2
 MTU=9000
+#VLANID=12
+VLANID=$( ip addr show eth1 | grep inet | head -1 | awk '{print $2}' | sed 's/\/.*$//' | sed -e "s/^.*\.//" )
 
 create_vlan() 
 {
@@ -12,7 +14,9 @@ create_vlan()
 	ip link set mtu ${MTU} dev ${INTERFACE}.${1}
 }
 
-create_vlan 1201 10.12.1.1/24
-create_vlan 1202 10.12.2.1/24
-create_vlan 1203 10.12.3.1/24
-create_vlan 1204 10.12.4.1/22
+create_vlan ${VLANID}01 10.${VLANID}.1.1/24
+create_vlan ${VLANID}02 10.${VLANID}.2.1/24
+create_vlan ${VLANID}03 10.${VLANID}.3.1/24
+
+# 10.12.8.0/21 = 10.12.8.0/24 + 10.12.10.0/23 + 10.12.12.0/22
+create_vlan ${VLANID}04 10.${VLANID}.8.1/21
