f9fc0c58777e28741f6c1643b5225dccd58ba0b2
Brice Dereims <bdereims@vmware.com>
2021-09-07 16:23:34 +0000

    mods after Madrid installation

--- list_cpod.sh
+++ list_cpod.sh
@@ -9,7 +9,7 @@ LHEADER=$( echo ${HEADER} | tr '[:upper:]' '[:lower:]' )
 main() {
 	echo "=== List of cPods"
 	for CPOD in $( cat ${HOSTS} | grep "${LHEADER}-" | awk '{print $2}' ); do
-		FIRST=$( cat /etc/hosts | grep -m 1 "${CPOD}\s" | sed "s/#//" | awk '$2 ~ /cpod-/ {gsub(/cpod-/,""); print toupper($2),"("$3")"}' )
+		FIRST=$( cat /etc/hosts | grep -m 1 "\/${CPOD}\s" | sed "s/#//" | awk '$2 ~ /cpod-/ {gsub(/cpod-/,""); print toupper($2),"("$3")"}' )
 		LAST=$( cat /etc/dnsmasq.conf | grep -m 1 "${CPOD}\." | sed -e "/start/d" -e "s/^.*=\///" -e "s/\/.*$//" )
 		FIRST="${FIRST} ......................................................."
 		FIRST=$( echo ${FIRST} | cut -c1-40 )
